24994169a7d45927aee2fa692257e2ad2b350de2
Andrew Lehmann <andrew@arlehmann.com>
2020-12-10 21:51:00 -0600

    cleanup Day10

--- src/Day10/Day10.rb
+++ src/Day10/Day10.rb
@@ -1,9 +1,3 @@
-# PART 1
-def getJoltageDiffs(joltages)
-  joltages.each_cons(2).map { |a,b| b-a}
-end
-
-
 def buildData(filename)
   input = File.readlines(filename).map { |line| line.to_i }.sort
   input << input.max + 3
@@ -12,20 +6,21 @@ def buildData(filename)
   input
 end
 
-data = buildData('input.txt')
-diffs = getJoltageDiffs(data)
+joltages = buildData('input.txt')
 
-diffsMap = Hash[diffs.uniq.map { |e| [e, diffs.count(e)] }]
+# PART 1
+joltageDiffs = joltages.each_cons(2).map { |a,b| b-a}
+joltageDiffsMap = Hash[joltageDiffs.uniq.map { |e| [e, joltageDiffs.count(e)] }]
 
-result = diffsMap[1] * diffsMap[3]
+result = joltageDiffsMap[1] * joltageDiffsMap[3]
 
-# puts result 
+puts result 
 
 
 # PART 2
-
 def checkNode(graph, node, destination, visited, numOfPaths = 0, memo = {})
   visited[node] = true;
+  
   if node == destination
     numOfPaths += 1
   elsif memo.has_key?(node)
@@ -40,15 +35,14 @@ def checkNode(graph, node, destination, visited, numOfPaths = 0, memo = {})
 
   visited[node] = false
   memo[node] = numOfPaths
-  return numOfPaths
+
+  numOfPaths
 end
 
-# Build list of edges in graph
-visitedNodes = Hash[data.map {|e| [e, false] }]
+possiblePaths = Hash[joltages.map { |e| [e, joltages.select { |f| f - e <= 3 and f - e > 0 }] }]
 
-possiblePaths = {}
-data.each { |e| possiblePaths[e] = data.select { |f| f - e <= 3 and f - e > 0 }.map {|f| f}}
-
-result2 = checkNode(possiblePaths, 0, data.max, visitedNodes)
-puts result2
 # Do DFS to find all possible paths
+visitedNodes = Hash[joltages.map {|e| [e, false] }]
+result2 = checkNode(possiblePaths, 0, joltages.max, visitedNodes)
+
+puts result2
