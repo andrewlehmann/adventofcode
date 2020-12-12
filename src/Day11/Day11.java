dddbbf593b230e2df077b3d99e90fb0cdb7ab17b
Andrew Lehmann <andrew@arlehmann.com>
2020-12-11 18:19:55 -0600

    Add Day 11 part 2, simplify

--- src/Day11/Day11.java
+++ src/Day11/Day11.java
@@ -3,132 +3,144 @@ package Day11;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-/**
- * Created by Drew on 12/8/2020.
- */
+/** Created by Drew on 12/8/2020. */
 public class Day11 {
-    public static void main(String[] args) throws FileNotFoundException {
-        List<List<Character>> seatingChart = new ArrayList<>();
-
-        try (Scanner s = new Scanner(new File("./src/Day11/input.txt"))) {
-            while (s.hasNextLine()) {
-                seatingChart.add(s.nextLine()
-                    .chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.toList()));
-            }
-            s.close();
+  private static boolean listsAreEqual(List<List<Character>> first, List<List<Character>> second) {
+    for (int i = 0; i < first.size(); i++) {
+      for (int j = 0; j < first.get(0).size(); j++) {
+        if (first.get(i).get(j) != second.get(i).get(j)) {
+          return false;
         }
-
-        seatingChart.add(0, new ArrayList<>());
-
-        for (int i = 0; i < seatingChart.get(1).size(); i++) {
-            seatingChart.get(0).add('.');
-        }
-
-        seatingChart.add(new ArrayList<>());
-
-        for (int i = 0; i < seatingChart.get(1).size(); i++) {
-            seatingChart.get(seatingChart.size() - 1).add('.');
-        }
-
-        seatingChart.stream().forEach(e -> {
-            e.add(0, '.');
-            e.add('.');
-        });
-
-
-        System.out.println(seatingChart);
-
-        int counter = 0;
-
-        List<List<Character>> prev = seatingChart;
-        List<List<Character>> next = processRules(seatingChart);
-        List<List<Character>> end = processRules(next);
-
-
-        while (!listsAreEqual(prev, next)) {
-            prev = next.stream().map(ArrayList::new).collect(Collectors.toList());
-            next = processRules(prev);
-
-            counter++;
-        }
-        System.out.println(seatingChart);
-        System.out.println(next);
-        System.out.println(end);
-        System.out.println(counter);
-
-        System.out.println(next.stream().flatMap(List::stream).filter(e -> e == '#').count());
-
+      }
     }
 
-    private static boolean listsAreEqual(List<List<Character>> first, List<List<Character>> second) {
-        for(int i = 0; i < first.size(); i++) {
-            for(int j = 0; j < first.get(0).size(); j++) {
-                if (first.get(i).get(j) != second.get(i).get(j)) {
-                    return false;
-                }
-            }
-        }
+    return true;
+  }
 
-        return true;
+  public static void main(String[] args) throws FileNotFoundException {
+    List<List<Character>> seatingChart = new ArrayList<>();
+
+    try (Scanner s = new Scanner(new File("./src/Day11/input.txt"))) {
+      while (s.hasNextLine()) {
+        seatingChart.add(s.nextLine().chars().mapToObj(c -> (char) c).collect(Collectors.toList()));
+      }
     }
 
+    int counter = 1;
+    List<List<Character>> prev = seatingChart;
+    List<List<Character>> next = processRules(seatingChart);
 
-    private static List<Character> adjacentElements(int i, int j, List<List<Character>> seatingChart) {
-        List<Character> adjacentToEntry = new ArrayList<>();
-
-        for (int x = (i > 0 ? -1 : 0); x <= (i < seatingChart.size() ? 1 : 0); x++) {
-            for (int y = (j > 0 ? -1 : 0); y <= (i < seatingChart.get(0).size() ? 1 : 0); y++) {
-                if (x != 0 || y != 0) {
-                    adjacentToEntry.add(seatingChart.get(i + x).get(j + y));
-                }
-            }
-        }
-
-        return adjacentToEntry;
-    }
-    private static char processEntry(int i, int j, List<List<Character>> seatingChart) {
-        char entry = seatingChart.get(i).get(j);
-
-        if (entry == '.') {
-            return '.';
-        }
-
-        List<Character> adjacentToEntry = adjacentElements(i, j, seatingChart);
-
-        if (entry == 'L') {
-            if (adjacentToEntry.stream().noneMatch(e -> e == '#')) {
-                return '#';
-            }
-
-            return 'L';
-        }
-
-        else {
-            if (adjacentToEntry.stream().filter(e -> e == '#').count() >= 4) {
-                return 'L';
-            }
-
-            return '#';
-        }
+    while (!listsAreEqual(prev, next)) {
+      prev = next.stream().map(ArrayList::new).collect(Collectors.toList());
+      next = processRules(prev);
+      counter++;
     }
 
-    private static List<List<Character>> processRules(List<List<Character>> seatingChart) {
-        List<List<Character>> newChart = new ArrayList<>();
+    System.out.println("Num times processed: " + counter);
 
-        for (int i = 0; i < seatingChart.size(); i++) {
-            newChart.add(new ArrayList<>());
-            for (int j = 0; j < seatingChart.get(0).size(); j++) {
-                char newValueForEntry = processEntry(i, j, seatingChart);
-                newChart.get(i).add(newValueForEntry);
-            }
+    System.out.println(
+        "Num seats occupied: " + next.stream().flatMap(List::stream).filter(e -> e == '#').count());
+  }
+
+  private static List<Character> getAdjacentElements(
+      int i, int j, List<List<Character>> seatingChart) {
+    List<Character> adjacentToEntry = new ArrayList<>();
+
+    for (int x = (i > 0 ? -1 : 0); x <= (i < seatingChart.size() - 1 ? 1 : 0); x++) {
+      for (int y = (j > 0 ? -1 : 0); y <= (j < seatingChart.get(0).size() - 1 ? 1 : 0); y++) {
+        if (x != 0 || y != 0) {
+          adjacentToEntry.add(seatingChart.get(i + x).get(j + y));
         }
-
-        return newChart;
+      }
     }
+
+    return adjacentToEntry;
+  }
+
+  private static char processEntry(int i, int j, List<List<Character>> seatingChart) {
+    char entry = seatingChart.get(i).get(j);
+
+    if (entry == '.') {
+      return '.';
+    }
+
+    List<Character> adjacentToEntry = getAdjacentElements(i, j, seatingChart);
+
+    if (entry == 'L') {
+      return adjacentToEntry.stream().noneMatch(e -> e == '#') ? '#' : 'L';
+    }
+
+    return adjacentToEntry.stream().filter(e -> e == '#').count() >= 4 ? 'L' : '#';
+  }
+
+  private static List<List<Character>> processRules(List<List<Character>> seatingChart) {
+    List<List<Character>> newChart = new ArrayList<>();
+
+    for (int i = 0; i < seatingChart.size(); i++) {
+      newChart.add(new ArrayList<>());
+
+      for (int j = 0; j < seatingChart.get(0).size(); j++) {
+        char newValueForEntry = processEntry2(i, j, seatingChart);
+        newChart.get(i).add(newValueForEntry);
+      }
+    }
+
+    return newChart;
+  }
+
+  // PART 2
+  private static boolean canSeeOccupiedSeat(
+      int i, int j, int slopeX, int slopeY, List<List<Character>> seatingChart) {
+    int x = i + slopeX;
+    int y = j + slopeY;
+
+    while (x >= 0 && y >= 0 && x < seatingChart.size() && y < seatingChart.get(0).size()) {
+      char seat = seatingChart.get(x).get(y);
+
+      switch (seat) {
+        case '#':
+          return true;
+        case 'L':
+          return false;
+        default:
+          x += slopeX;
+          y += slopeY;
+      }
+    }
+
+    return false;
+  }
+
+  private static List<Boolean> getLineOfSight(int i, int j, List<List<Character>> seatingChart) {
+    return Arrays.asList(
+        canSeeOccupiedSeat(i, j, 1, 1, seatingChart),
+        canSeeOccupiedSeat(i, j, 1, -1, seatingChart),
+        canSeeOccupiedSeat(i, j, -1, 1, seatingChart),
+        canSeeOccupiedSeat(i, j, -1, -1, seatingChart),
+        canSeeOccupiedSeat(i, j, 0, 1, seatingChart),
+        canSeeOccupiedSeat(i, j, 0, -1, seatingChart),
+        canSeeOccupiedSeat(i, j, 1, 0, seatingChart),
+        canSeeOccupiedSeat(i, j, -1, 0, seatingChart));
+  }
+
+  private static char processEntry2(int i, int j, List<List<Character>> seatingChart) {
+    char entry = seatingChart.get(i).get(j);
+
+    if (entry == '.') {
+      return '.';
+    }
+
+    List<Boolean> lineOfSight = getLineOfSight(i, j, seatingChart);
+
+    if (entry == 'L') {
+      return lineOfSight.stream().noneMatch(e -> e) ? '#' : 'L';
+    }
+
+    return lineOfSight.stream().filter(e -> e).count() >= 5 ? 'L' : '#';
+  }
 }
