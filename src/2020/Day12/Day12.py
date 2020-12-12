991ad262918cf92cc6627696d8ddfd1d67f07230
Andrew Lehmann <andrew@arlehmann.com>
2020-12-12 01:45:33 -0600

    Update Day12 for part2

--- src/2020/Day12/Day12.py
+++ src/2020/Day12/Day12.py
@@ -1,4 +1,31 @@
-import math  
+import math
+
+
+def rotate(x, y, direction_to_rotate, degrees_to_rotate):
+
+    if x == 0 and y == 0:
+      return (0, 0)
+    elif x != 0:
+      curr_direction = round(math.atan(math.radians(x / y)) / 90) * 90
+    else: 
+      curr_direction = 90 if y > 0 else 270
+
+    if direction_to_rotate == 'L':
+        new_direction = (curr_direction + degrees_to_rotate) % 360
+    elif direction_to_rotate == 'R':
+        new_direction = (curr_direction - degrees_to_rotate) % 360
+
+    if new_direction == 90:
+        return (-y, x)
+    elif new_direction == 180:
+        return (-x, -y)
+    elif new_direction == 270:
+        return (y, -x)
+    elif new_direction == 0:
+        return (x, y)
+    else:
+      raise ValueError("Bad direction: " + direction_to_rotate)
+
 
 input_data = open("src/2020/Day12/input.txt")
 lines = input_data.readlines()
@@ -8,33 +35,34 @@ print(actions)
 location_x = 0
 location_y = 0
 
-direction_facing = 0
+waypoint_x = 10
+waypoint_y = 1
 
 for action in actions:
     action_type = action[0]
     value = int(action[1:])
 
     if action_type == 'L':
-        direction_facing = (direction_facing + value) % 360
+        waypoint_x, waypoint_y = rotate(waypoint_x, waypoint_y, action_type, value)
 
     elif action_type == 'R':
-        direction_facing = (direction_facing - value) % 360
+        waypoint_x, waypoint_y = rotate(waypoint_x, waypoint_y, action_type, value)
 
     elif action_type == "N":
-        location_y += value
+        waypoint_y += value
 
     elif action_type == "S":
-        location_y -= value
+        waypoint_y -= value
 
     elif action_type == "E":
-        location_x += value
+        waypoint_x += value
 
     elif action_type == "W":
-        location_x -= value
+        waypoint_x -= value
 
     elif action_type == 'F':
-      location_x = location_x + value * math.cos(math.radians(direction_facing))
-      location_y = location_y + value * math.sin(math.radians(direction_facing))
+        location_x = location_x + value * waypoint_x
+        location_y = location_y + value * waypoint_y
 
 
 print(location_x, location_y)
